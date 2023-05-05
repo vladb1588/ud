f1175430d67e1b8276ec6a3d384d3615b87ec69a
vladb1588 <vladbeldze@gmail.com>
2023-05-05 22:24:29 +0700

    comit 6

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std; int d;//глобальная
+int f; //глобальная
+int main()
+{
+	string s1 = "HI";
+	string s2 = "vi";
+	const int g = 5; git pull origin main --allow - unrelated - histories
+	const int h = 7;
+	int d = 2;
+	int f = 5;
+	int a = 1;
+	int s = 2;
+	cout << d << endl;//глобàдьная
+	cout << f << endl;//глобадьная
+	cout << a << endl;//локальная
+	cout << s << endl;//локальная
+	cout << g << endl; //KOHCтанта 
+	cout << g << endl; //кOнстанта
+	cout << h << endl; //KOнстaнта
+	cout << s1 << endl; //cтpoкa 
+	cout << s2 << endl; //cтpoka
+	return 0;
+}
